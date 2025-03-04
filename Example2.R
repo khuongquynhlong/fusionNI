6f3231b2cd03e2ab9439b642716101b46b7fb037
longkhuong <longkhuong.yds@gmail.com>
2025-03-04 18:07:56 -0500

    add simulation and SE functions

--- Example2.R
+++ Example2.R
@@ -61,6 +61,7 @@ sim_data <- function(n1, n2, seed = NULL) {
 }
 
 
+
 #----- Weight generating function
 #===============================================================================
 gen_weight <- function(data) {
@@ -69,20 +70,20 @@ gen_weight <- function(data) {
   a_mod       <- glm(A ~ X1 + X2, family = "binomial", data = distal_data)
   a_pred      <- predict(a_mod, newdata = data, type = "response")
   w_a         <- 1/a_pred
-
+  
   # W_trans
   s_mod       <- glm(S ~ X1 + X2, family = "binomial", data = data)
   s_pred      <- predict(s_mod, type = "response")
   w_trans     <- s_pred/(1-s_pred)
   w_trial     <-  (1 - mean(data$S))/mean(data$S)
   w_2t        <- w_trans*w_trial
-
+  
   # All components
   w_all        <- w_a*w_2t
   data$w_trans <- w_trans
   data$w_2t    <- w_2t
   data$w_all   <- w_all
-
+  
   return(data)
 }
 
@@ -104,25 +105,44 @@ gen_weight_s <- function(data) {
 
 #----- Estimates from local and distal trials separately
 trial_est <- function(data){
-  dataS0 <- subset(data, S == 0)
-  dataS1 <- subset(data, S == 1)
+  Y12 <- data[data$S == 1 & data$A == 2, ]$Y
+  Y11 <- data[data$S == 1 & data$A == 1, ]$Y
+  Y01 <- data[data$S == 0 & data$A == 1, ]$Y
+  Y00 <- data[data$S == 0 & data$A == 0, ]$Y
   
   # psi_local
-  psi_local1 <- mean(dataS1[dataS1$A == 1, ]$Y)
-  psi_local2 <- mean(dataS1[dataS1$A == 2, ]$Y)
+  psi_local1 <- mean(Y11)
+  psi_local2 <- mean(Y12)
   psi_local  <- psi_local1 - psi_local2
   
+  # Binomial se for psi
+  se_psi_local1 <- sqrt((psi_local1 * (1 - psi_local1))/length(Y11))
+  se_psi_local2 <- sqrt((psi_local2 * (1 - psi_local2))/length(Y12))
+  se_psi_local  <- sqrt(se_psi_local1^2 + se_psi_local2^2)
+  
   # psi_distal
-  psi_distal1 <- mean(dataS0[dataS0$A == 1, ]$Y) 
-  psi_distal0 <- mean(dataS0[dataS0$A == 0, ]$Y)
+  psi_distal1 <- mean(Y01) 
+  psi_distal0 <- mean(Y00)
   psi_distal  <- psi_distal1 - psi_distal0
   
-  est_list <- list(psi_local1  = psi_local1, 
-                   psi_local2  = psi_local2, 
-                   psi_local   = psi_local,
-                   psi_distal0 = psi_distal0,
-                   psi_distal1 = psi_distal1,
-                   psi_distal  = psi_distal)
+  # Binomial se for psi
+  se_psi_distal1 <- sqrt((psi_distal1 * (1 - psi_distal1))/length(Y01))
+  se_psi_distal0 <- sqrt((psi_distal0 * (1 - psi_distal0))/length(Y00))
+  se_psi_distal  <- sqrt(se_psi_distal1^2 + se_psi_distal0^2)
+  
+  
+  est_list <- list(psi_local1     = psi_local1, 
+                   psi_local2     = psi_local2, 
+                   psi_local      = psi_local,
+                   psi_distal0    = psi_distal0,
+                   psi_distal1    = psi_distal1,
+                   psi_distal     = psi_distal,
+                   se_psi_local1  = se_psi_local1,
+                   se_psi_local2  = se_psi_local2,
+                   se_psi_local   = se_psi_local,
+                   se_psi_distal1 = se_psi_distal1,
+                   se_psi_distal0 = se_psi_distal0,
+                   se_psi_distal  = se_psi_distal)
   return(est_list)
 }
 
@@ -133,7 +153,7 @@ trans_est <- function(data){
   dataS0     <- subset(data, S == 0)
   # Applying the weight
   Y1w_mod    <- svyglm(Y ~ A, family = "quasibinomial", 
-                    design = svydesign(~ 1, weights = ~dataS0$w_trans, data = dataS0))
+                       design = svydesign(~ 1, weights = ~dataS0$w_trans, data = dataS0))
   pY1        <- predict(Y1w_mod, newdata = data.frame(A = 1), type = "response") 
   pY0        <- predict(Y1w_mod, newdata = data.frame(A = 0), type = "response") 
   
@@ -141,9 +161,19 @@ trans_est <- function(data){
   psi_trans0 <- mean(pY0)
   psi_trans  <- psi_trans1 - psi_trans0
   
-  est_list   <- list(psi_trans0 = psi_trans0,
-                     psi_trans1 = psi_trans1, 
-                     psi_trans = psi_trans)
+  # SE
+  # `svyglm` always returns robust standard errors
+  # ~ equivalent to se via influence function
+  se_psi_trans1 <- as.numeric(SE(pY1))
+  se_psi_trans0 <- as.numeric(SE(pY0))
+  se_psi_trans  <- sqrt(se_psi_trans1^2 + se_psi_trans0^2)
+  
+  est_list   <- list(psi_trans0    = psi_trans0,
+                     psi_trans1    = psi_trans1, 
+                     psi_trans     = psi_trans,
+                     se_psi_trans1 = se_psi_trans1,
+                     se_psi_trans0 = se_psi_trans0,
+                     se_psi_trans  = se_psi_trans)
   return(est_list)
 }
 
@@ -157,38 +187,89 @@ sim_combine <- function(data){
   dataS1      <- subset(data, S == 1)
   
   # True values
-  true_Y2     <- mean(dataS1$Y2)
-  true_Y0     <- mean(dataS1$Y0)
-  true_fusion <- true_Y2 - true_Y0
+  true_Y2       <- mean(dataS1$Y2)
+  true_Y0       <- mean(dataS1$Y0)
+  true_fusion   <- true_Y2 - true_Y0
   
   # Estimates
-  trial_list  <- trial_est(data)
-  trans_list  <- trans_est(data)
+  trial_list    <- trial_est(data)
+  trans_list    <- trans_est(data)
   
   # Fusion estimate
-  psi_fusion  <- trans_list$psi_trans - trial_list$psi_local
- 
+  psi_fusion    <- trans_list$psi_trans - trial_list$psi_local
+  se_psi_fusion <- sqrt(trans_list$se_psi_trans^2 + trial_list$se_psi_local^2)
+  
   # Crude estimate 
-  psi_crude   <- trial_list$psi_local2 - trial_list$psi_distal0
+  psi_crude     <- trial_list$psi_local2 - trial_list$psi_distal0
+  se_psi_crude  <- sqrt(trial_list$se_psi_local2^2 + trial_list$se_psi_distal0^2)
   
   # Difference between distal and trans for active control (A = 1)
-  C_diff      <- trans_list$psi_trans1 - trial_list$psi_distal1
+  C_diff        <- trans_list$psi_trans1 - trial_list$psi_distal1
   
   # Bias: absolute
-  bias_fusion <- true_fusion - psi_fusion
-  bias_crude  <- true_fusion - psi_crude
+  bias_fusion   <- true_fusion - psi_fusion
+  bias_crude    <- true_fusion - psi_crude
   
-  est_list    <- data.frame(true_fusion = true_fusion,
-                            psi_fusion  = psi_fusion, 
-                            psi_crude   = psi_crude,
-                            C_diff      = C_diff,
-                            bias_fusion = bias_fusion,
-                            bias_crude  = bias_crude)
+  est_list      <- data.frame(true_fusion   = true_fusion,
+                              psi_fusion    = psi_fusion, 
+                              psi_crude     = psi_crude,
+                              C_diff        = C_diff,
+                              bias_fusion   = bias_fusion,
+                              bias_crude    = bias_crude,
+                              se_psi_fusion = se_psi_fusion,
+                              se_psi_crude  = se_psi_crude)
   return(est_list)
 }
 
 
 
+
+#----- Bootstrap
+#===============================================================================
+sim_boot <- function(B, data) {
+  # Subset the data by S and A groups
+  dataS1A2 <- subset(data, S == 1 & A == 2)
+  dataS1A1 <- subset(data, S == 1 & A == 1)
+  dataS0A1 <- subset(data, S == 0 & A == 1)
+  dataS0A0 <- subset(data, S == 0 & A == 0)
+  
+  # Get the number of observations
+  n12 <- nrow(dataS1A2)
+  n11 <- nrow(dataS1A1)
+  n01 <- nrow(dataS0A1)
+  n00 <- nrow(dataS0A0)
+  
+  # List to store bootstrap results
+  boot_results <- vector("list", B)
+  
+  for (i in 1:B) {
+    # Resample with replacement from each group
+    dataS1A2_b <- slice_sample(dataS1A2, n = n12, replace = TRUE)
+    dataS1A1_b <- slice_sample(dataS1A1, n = n11, replace = TRUE)
+    dataS0A1_b <- slice_sample(dataS0A1, n = n01, replace = TRUE)
+    dataS0A0_b <- slice_sample(dataS0A0, n = n00, replace = TRUE)
+    
+    # Combine the bootstrap samples
+    data_boot <- rbind(dataS1A2_b, dataS1A1_b, dataS0A1_b, dataS0A0_b)
+    
+    # Calculate estimates on the bootstrap sample
+    temp_result <- sim_combine(data_boot)
+    
+    # Store the result
+    boot_results[[i]] <- temp_result
+  }
+  
+  # Combine all bootstrap results into one data frame
+  boot_df <- do.call(rbind, boot_results)
+  return(boot_df)
+}
+
+
+
+
+#===============================================================================
+#------------------------------- Run simulation -------------------------------- 
+#===============================================================================
 
 num_cores <- parallel::detectCores() - 4
 
@@ -198,7 +279,7 @@ registerDoParallel(cl)
 # Define simulation grid parameters
 n1_vals    <- c(200, 500, 1000, 2000)
 n2_vals    <- c(200, 500, 1000, 2000)
-iterations <- 100000
+iterations <- 10000
 
 # Create a grid of parameter combinations
 param_grid <- expand.grid(n1 = n1_vals, n2 = n2_vals)
@@ -243,15 +324,7 @@ runtime <- end_time - start_time
 runtime
 
 
-# saveRDS(result_tab, "Example2.RDS")
-
-
-result_tab <- readRDS("Example2.RDS")
-
-
-
-
-
+# saveRDS(result_tab, "Example2_est.RDS")
 
 # Probability scale
 result_sum <- result_tab |>
@@ -263,43 +336,244 @@ result_sum <- result_tab |>
     C_diff        = mean(C_diff),
     mbias_fusion  = mean(bias_fusion),
     mbias_crude   = mean(bias_crude),
+    se_ave_fusion = mean(se_psi_fusion),
+    se_ave_crude  = mean(se_psi_crude),
     rmse_fusion   = sqrt(mean(bias_fusion^2)),
     rmse_crude    = sqrt(mean(bias_crude^2)),
     fusion_mce    = sd(bias_fusion),
     crude_mce     = sd(bias_crude)
-  ) |>
-  mutate_at(c("mbias_fusion", "mbias_crude", "rmse_fusion",
-              "rmse_crude", "fusion_mce", "crude_mce"), 
-            function(x){x = round(x, 4)})
+  ) |> mutate_all(function(x){x = round(x, 4)})
 
-writexl::write_xlsx(result_sum, "result_sum.xlsx")
+# writexl::write_xlsx(result_sum, "result_sum.xlsx")
 
 
 
-# Percentage scale
-result_per <- result_tab |>
-  mutate(
-    bias_fusion   = bias_fusion*100,
-    bias_crude    = bias_crude*100) |>
+
+
+
+#===============================================================================
+#------------------------------ Inference: 95% CI ------------------------------ 
+#===============================================================================
+
+#----- Finding coverage of Asymptotic 95%CI
+#===============================================================================
+result_tab <- readRDS("Example2_est.RDS")
+
+# Probability scale
+param_grid <- result_tab |>
   group_by(n1, n2) |>
   summarise(
-    true_fusion   = mean(true_fusion*100),
-    psi_fusion    = mean(psi_fusion*100),
-    psi_crude     = mean(psi_crude*100),
-    C_diff        = mean(C_diff*100),
-    mbias_fusion  = mean(bias_fusion),
-    mbias_crude   = mean(bias_crude),
-    rmse_fusion   = sqrt(mean(bias_fusion^2)),
-    rmse_crude    = sqrt(mean(bias_crude^2)),
-    fusion_mce    = sd(bias_fusion),
-    crude_mce     = sd(bias_crude)
-  ) |>
-  mutate_at(c("mbias_fusion", "mbias_crude", "rmse_fusion",
-              "rmse_crude", "fusion_mce", "crude_mce"), 
-            function(x){x = round(x, 4)})
+    true_fusion   = mean(true_fusion),
+    psi_fusion    = mean(psi_fusion)) |>
+  ungroup()
+
+# Number of Monte Carlo replicates
+MC <- 10000
+
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+start_time <- Sys.time()
+
+set.seed(12345)
+
+# Parallel loop
+coverage_results <- foreach(i = 1:nrow(param_grid), .combine = rbind, 
+                            .packages = c("survey", "dplyr")) %dopar% {
+                              
+                              n1 <- param_grid$n1[i]
+                              n2 <- param_grid$n2[i]
+                              true_fusion <- param_grid$true_fusion[i]
+                              
+                              cover_fusion  <- 0
+                              cover_crude  <- 0
+                              
+                              # Run MC replicates for this combination
+                              for (j in 1:MC) {
+                                # Simulate data and generate weights
+                                data_sim <- sim_data(n1 = n1, n2 = n2) |> gen_weight()
+                                
+                                sim_re <- sim_combine(data_sim)
+                                
+                                # Calculate the asymptotic 95% CI for the fusion estimator
+                                lower_ci <- sim_re$psi_fusion - 1.96 * sim_re$se_psi_fusion
+                                upper_ci <- sim_re$psi_fusion + 1.96 * sim_re$se_psi_fusion
+                                
+                                lower_ci_cr <- sim_re$psi_crude - 1.96 * sim_re$se_psi_crude
+                                upper_ci_cr <- sim_re$psi_crude + 1.96 * sim_re$se_psi_crude
+                                
+                                # Check whether the true fusion effect falls inside the CI
+                                
+                                if (true_fusion >= lower_ci && true_fusion <= upper_ci) {
+                                  cover_fusion <- cover_fusion + 1
+                                }
+                                
+                                # Crude 
+                                
+                                if (true_fusion >= lower_ci_cr && true_fusion <= upper_ci_cr) {
+                                  cover_crude <- cover_crude + 1
+                                }
+                                
+                              }
+                              
+                              # Compute the coverage probability
+                              cover_fusion <- cover_fusion / MC
+                              
+                              cover_crude <- cover_crude / MC
+                              
+                              data.frame(n1 = n1, n2 = n2, 
+                                         cover_fusion = cover_fusion,
+                                         cover_crude = cover_crude)
+                            }
+
+stopCluster(cl)
+
+# Runtime
+end_time <- Sys.time()
+runtime <- end_time - start_time
+runtime
+
+
+saveRDS(coverage_results, "coverage_results.RDS")
 
 
 
 
 
 
+
+
+#----- Bootstrap SE: averaged over 10,000 runs
+#===============================================================================
+result_tab <- readRDS("Example2_est.RDS")
+
+# Probability scale
+param_grid <- result_tab |>
+  group_by(n1, n2) |>
+  summarise(
+    true_fusion   = mean(true_fusion),
+    psi_fusion    = mean(psi_fusion)) |>
+  ungroup()
+
+# Number of Monte Carlo replicates
+MC <- 90
+
+cl <- makeCluster(detectCores() - 4)
+registerDoParallel(cl)
+
+start_time <- Sys.time()
+
+set.seed(12345)
+
+results_boot_se <- foreach(i = 1:nrow(param_grid), .combine = rbind,
+                           .packages = c("survey", "dplyr")) %dopar% {
+                             n1 <- param_grid$n1[i]
+                             n2 <- param_grid$n2[i]
+                             
+                             boot_se_fusion <- numeric(MC)
+                             boot_se_crude  <- numeric(MC)
+                             
+                             for (j in 1:MC) {
+                               # Simulate data and generate weights
+                               data_sim <- sim_data(n1 = n1, n2 = n2) |> gen_weight()
+                               
+                               # Run the bootstrap procedure 
+                               boot_re <- sim_boot(B = 2000, data = data_sim)
+                               
+                               boot_se_fusion[j] <- sd(boot_re$psi_fusion)
+                               boot_se_crude[j]  <- sd(boot_re$psi_crude)
+                             }
+                             
+                             # Compute the average bootstrap SE for the current (n1, n2) combination
+                             boot_se_fusion_ave <- mean(boot_se_fusion)
+                             boot_se_crude_ave <- mean(boot_se_crude)
+                             
+                             # Return a data frame row with the sample sizes and average bootstrap SE
+                             data.frame(n1 = n1, n2 = n2, 
+                                        boot_se_fusion = boot_se_fusion,
+                                        boot_se_crude = boot_se_crude)
+                           }
+
+
+stopCluster(cl)
+
+# Runtime
+end_time <- Sys.time()
+runtime <- end_time - start_time
+runtime
+
+
+saveRDS(results_boot_se, "results_boot_se100.RDS")
+
+
+
+
+
+
+#----- Compare 95%CI: Supplemental Table 1
+#===============================================================================
+n1_values <- c(200, 500, 1000, 2000)
+n2_values <- c(200, 500, 1000, 2000)
+param_grid <- expand.grid(n1 = n1_values, n2 = n2_values)
+
+cl <- makeCluster(detectCores() - 2)
+registerDoParallel(cl)
+
+start_time <- Sys.time()
+
+set.seed(12345)
+# Parallel loop over each parameter combination
+boot_results <- foreach(i = 1:nrow(param_grid), .combine = rbind, 
+                        .packages = c("survey", "dplyr")) %dopar% {
+                          n1 <- param_grid$n1[i]
+                          n2 <- param_grid$n2[i]
+                          
+                          data_sim <- sim_data(n1 = n1, n2 = n2) |> gen_weight()
+                          
+                          # Obtain the combined estimates
+                          sim_re <- sim_combine(data_sim)
+                          
+                          # Perform the bootstrap
+                          boot_re <- sim_boot(B = 5000, data = data_sim)
+                          
+                          # Asymptotic 95%CI
+                          fusion_asymp_lower <- sim_re$psi_fusion - 1.96 * sim_re$se_psi_fusion
+                          fusion_asymp_upper <- sim_re$psi_fusion + 1.96 * sim_re$se_psi_fusion
+                          
+                          crude_asymp_lower <- sim_re$psi_crude - 1.96 * sim_re$se_psi_crude
+                          crude_asymp_upper <- sim_re$psi_crude + 1.96 * sim_re$se_psi_crude
+                          
+                          # Bootstrap 95%CI
+                          fusion_boot_CI <- quantile(boot_re$psi_fusion, probs = c(0.025, 0.975))
+                          crude_boot_CI  <- quantile(boot_re$psi_crude, probs = c(0.025, 0.975))
+                          
+                          re_list <- data.frame(
+                            n1 = n1,
+                            n2 = n2,
+                            psi_fusion         = sim_re$psi_fusion,
+                            fusion_asymp_se    = sim_re$se_psi_fusion,
+                            fusion_asymp_lower = fusion_asymp_lower,
+                            fusion_asymp_upper = fusion_asymp_upper,
+                            fusion_boot_se     = sd(boot_re$psi_fusion),
+                            fusion_boot_lower  = fusion_boot_CI[1],
+                            fusion_boot_upper  = fusion_boot_CI[2],
+                            psi_crude          = sim_re$psi_crude,
+                            crude_asymp_se     = sim_re$se_psi_crude,
+                            crude_asymp_lower  = crude_asymp_lower,
+                            crude_asymp_upper  = crude_asymp_upper,
+                            crude_boot_se      = sd(boot_re$psi_crude),
+                            crude_boot_lower   = crude_boot_CI[1],
+                            crude_boot_upper   = crude_boot_CI[2]
+                          )
+                          
+                          return(re_list)
+                        }
+
+stopCluster(cl)
+
+# Runtime
+end_time <- Sys.time()
+runtime <- end_time - start_time
+runtime
+
+saveRDS(boot_results, "boot_results.RDS")
